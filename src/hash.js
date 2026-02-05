1b2836fcc676d4155bd9cb10fb9758e8b755b3ff
ggihan <140435642+GGihan@users.noreply.github.com>
2026-02-05 21:57:35 +0100

    Add all remaining HashMap methods.

--- src/hash.js
+++ src/hash.js
@@ -17,9 +17,17 @@ export class HashMap {
         return hashCode;
     }
 
-    set(key, value) {
+    _getBucket(key) {
         const index = this.hash(key);
-        const bucket = this.buckets[index];
+
+        if (index < 0 || index >= this.buckets.length) {
+            throw new Error("Trying to access index out of bounds");
+        }
+        return this.buckets[index];
+    }
+
+    set(key, value) {
+        const bucket = this._getBucket(key);
 
         for (let i = 0; i < bucket.length; i++) {
             const entry = bucket[i];
@@ -31,11 +39,14 @@ export class HashMap {
 
         bucket.push([key, value]);
         this.size++;
+
+        if (this.size / this.capacity > this.loadFactor) {
+            this.resize();
+        }
     }
 
     get(key) {
-        const index = this.hash(key);
-        const bucket = this.buckets[index];
+        const bucket = this._getBucket(key);
 
         for (let i = 0; i < bucket.length; i++) {
             const [storedKey, value] = bucket[i];
@@ -45,5 +56,97 @@ export class HashMap {
         }
 
         return null;
+    }
+
+    has(key) {
+        const bucket = this._getBucket(key);
+
+        for (let i = 0; i < bucket.length; i++) {
+            if (bucket[i][0] === key) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    remove(key) {
+        const bucket = this._getBucket(key);
+
+        for (let i = 0; i < bucket.length; i++) {
+            if (bucket[i][0] === key) {
+                bucket.splice(i, 1);
+                this.size--;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    length() {
+        return this.size;
+    }
+
+    clear() {
+        this.capacity = 16;
+        this.size = 0;
+        this.buckets = new Array(this.capacity).fill(null).map(() => []);
+    }
+
+    keys() {
+        const keysArray = [];
+
+        for (let i = 0; i < this.buckets.length; i++) {
+            const bucket = this.buckets[i];
+
+            for (let j = 0; j < bucket.length; j++) {
+                const [key] = bucket[j];
+                keysArray.push(key);
+            }
+        }
+
+        return keysArray;
+    }
+
+    values() {
+        const valuesArray = [];
+
+        for (let i = 0; i < this.buckets.length; i++) {
+            const bucket = this.buckets[i];
+
+            for (let j = 0; j < bucket.length; j++) {
+                const [, value] = bucket[j];
+                valuesArray.push(value);
+            }
+        }
+
+        return valuesArray;
+    }
+
+    entries() {
+        const pairArray = [];
+
+        for (let i = 0; i < this.buckets.length; i++) {
+            const bucket = this.buckets[i];
+
+            for (let j = 0; j < bucket.length; j++) {
+                pairArray.push(bucket[j]);
+            }
+        }
+
+        return pairArray
+    }
+
+    resize() {
+        const oldEntries = this.entries();
+        this.capacity *= 2;
+        this.size = 0;
+
+        this.buckets = new Array(this.capacity).fill(null).map(() => []);
+
+        for (const [key, value] of oldEntries) {
+            this.set(key, value);
+        }
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import { HashMap } from "./hash";
 import "./styles.css";
 
-console.log('Hello from index.js!');
+const test = new HashMap();
+window.test = test;
+
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+
